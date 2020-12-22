c63398802292c7b312eb88f3aaa71ca1cd618e40
Jkochanik <jkochani@stevens.edu>
2020-12-22 02:00:21 -0500

    comments

--- era.s
+++ era.s
@@ -21,10 +21,10 @@ _Z12eratosthenesPjj:
 	
 
 	mov	r2, #0xFFFFFFFF		@r2 is register to set all true
-	lsr	r1, #1
 	mov	r4, r0 			@r4 is long term save for array address
 	add	r3, r1, #32		@Find counter to set true by 32 bits and keep in r3. Step 1: add by 32
 	lsr	r3, #6			@Step 2: divide n by 64
+	lsr	r1, #1
 setAllPrime:	
 	str	r2, [r0], #4		@store FFFFFFFF in every 32 bits 
 	subs	r3, #4			@index counter for FFs filler subs to count down
@@ -46,40 +46,40 @@ getbit:
 
 
 
-	lsl	r8, r3, #1
-	add	r8, #1		@turns r8 into number counter for bit clears
-	add	r9, r8
+	lsl	r8, r3, #1		@gets value of actual number 
+	add	r8, #1			@turns r8 into number counter for bit clears
+	add	r9, r8			@odd multiples are spaced the value distance apart
 bitClear:	
 	
-	lsr	r0, r9, #5
+	lsr	r0, r9, #5		@same algorithm from above to find what word index its on
 	lsl	r0, #2
 	ldr	r10, [r4, r0]		@load word based on where r9 is			 
-	and	r5, r9, #31
+	and	r5, r9, #31		@same bit selecting algorithm from above
 	lsl	r5, r6, r5
 	tst	r10, r5
-	bicne	r10, r5
-	str	r10, [r4, r0]
-	add	r9, r8
-	cmp	r1, r9
+	bicne	r10, r5			@actual bit clear
+	str	r10, [r4, r0]		@store modified word
+	add	r9, r8			@iterate to next odd multiple of r8
+	cmp	r1, r9			@check if multiples were accounted for
 	bge	bitClear
-	mov	r9, #0
+	mov	r9, #0			@set r9 to zero so process knows it's no longer bit clearing
 	
 	
 	
 looper:
-	add	r3, #1
-	cmp	r3, r1
-	beq	end
-	lsr	r0, r3, #5
-	cmp	r9, #0
-	beq	getWord
-	cmp	r11, r0 
-	beq	getbit
-	b	getWord
+	add	r3, #1			@iterate to next position
+	cmp	r3, r1			@check if your done with all numbers
+	beq	end			@end if done with all numbers
+	lsr	r0, r3, #5		@used to see if on a new word
+	cmp	r9, #0			@check if just finished a bit clear iteration
+	beq	getWord			@get new word after bit clearing to be safe
+	cmp	r11, r0 		@check if on a new word
+	beq	getbit			@if not on new word and didn't start bitclearing just use word already in r2
+	b	getWord			@fail safe get new word if in doubt lol
 
-end:	
+end:				@add up all prime numbers
 	mov	r0, #0		@sum
-	mov	r3, #1		@position index
+	mov	r3, #0		@position index
 endloop:
 	mov	r5, #32		@word length count down
 	ldr	r2, [r4], #4
@@ -92,13 +92,14 @@ endloop2:
 	add	r3, #1
 	beq 	endloop		@branch to get new word
 	cmp	r1, r3		@count down to see when finished entire array
-	bne	endloop2	@if not finished loop again
+	bge	endloop2	@if not finished loop again
 
 
 	pop	{r10, r11}
 	pop	{r8, r9}
 	pop	{r6, r7}
 	pop	{r4, r5}
+	sub	r0, #1		@ignore 0
 	bx 	lr
 	
 	
